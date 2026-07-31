e3bca84dfe2da1ca496b210e82d58a6059ba08c9
ahmed-nassr <iiahmednassr@gmail.com>
2026-07-31 13:08:55 +0300

    Data Cleaning Done

--- src/calculators.py
+++ src/calculators.py
@@ -1,9 +1,19 @@
-def proportion(part, total):
+def percent(part, total):
     """
 
-    Calculates the proportion of two containers.
+    Calculate `part` as a percentage from `total`.
     
     """
-    proportion = (len(part) / len(total)) * 100
-    rounded_proportion = round(proportion, 2)
-    return str(rounded_proportion) + '%'
+    if total == 0:
+        raise ValueError("Total cannot be zero.")
+    elif total < 0 or part < 0:
+        raise ValueError("Input cannot be negative")
+    if part == 0:
+        return "0%"
+    percentage = (part / total) * 100
+    if round(percentage, 2) > 0:
+        rounded_percentage = round(percentage, 2)
+        return str(rounded_percentage) + '%'
+    else:
+        new_percentage = format(percentage, '.0e')
+        return new_percentage + '%'
